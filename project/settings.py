d74b6eb1714d117ebb958a581f1a7a474972194e
kerstenleeeee <leekristineclair@gmail.com>
2019-01-24 17:47:24 +0800

    update

--- project/settings.py
+++ project/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'maroon_api.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'maroon_api.wsgi.application'
 
 
 # Database
