d90d7831c23d807c2d187913f29a9a0f28a009cd
kerstenleeeee <leekristineclair@gmail.com>
2019-02-05 20:56:37 +0800

    staticfiles

--- maroon_api/settings.py
+++ maroon_api/settings.py
@@ -140,6 +140,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/maroonprint-app/pages/static'
