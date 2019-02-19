2e6b9d937ad7d0ca7fc1b2b3232aea4f82e628bd
hannahmagno <hannah.magno15@gmai.com>
2019-02-19 15:15:34 +0800

    added license and comments

--- bps/forms.py
+++ bps/forms.py
@@ -1,3 +1,17 @@
+# License
+# Author: Hannah Mae Magno
+# This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo 
+# of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2018-2019 
+
+# Code History
+# Lee - 02/17/19 - Added the forms for add functionality
+# Lee - 02/18/19 - Final commit
+
+# File creation date: 02/17/19
+# Development Group: 3
+# Client Group: 3
+# Description: Forms for the add blueprint functionality
+
 from django import forms
 from .models import Building, Floor
 
